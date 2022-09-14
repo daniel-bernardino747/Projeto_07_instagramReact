39bfb58a6571c3a152d8ecd9df0609bdde982dde
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-09-14 15:57:45 -0300

    SideBar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,18 +1,95 @@
+const UsersAccounts = [
+    <Item
+        isSuggestion={false}
+        url="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
+        name="Nome Oficial Usuário"
+        subtitle="Nome Usuário editável"
+    />,
+    <Item
+        isSuggestion={true}
+        url="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
+        name="Nome Sugestão"
+        subtitle="Nome oficial"
+    />,
+    <Item
+        isSuggestion={true}
+        url="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
+        name="Nome Sugestão"
+        subtitle="Nome oficial"
+    />,
+    <Item
+        isSuggestion={true}
+        url="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
+        name="Nome Sugestão"
+        subtitle="Nome oficial"
+    />
+]
+
+function Template(props) {
+    return (
+        <div className="u-center-x u-display-flex">
+            <div>
+                <img
+                    src={props.image}
+                    alt="User Perfil Image"
+                />
+            </div>
+            <div>
+                <h1>{props.name}</h1>
+                <h2>{props.subtitle}</h2>
+            </div>
+        </div>
+    )
+}
+
+function Item(props) {
+    const isSuggestion = props.isSuggestion;
+
+    if (isSuggestion) {
+        return (
+            <div className="u-space-between-center u-display-flex">
+                <Template
+                    image={props.url}
+                    name={props.name}
+                    subtitle={props.subtitle}
+                />
+                <h3>Seguir</h3>
+            </div>
+        );
+    }
+
+    return (
+        <Template
+            image={props.url}
+            name={props.name}
+            subtitle={props.subtitle}
+        />
+    );
+}
+
+
 export default function SideBar() {
+
+    const currentUser = UsersAccounts.filter(user => user.props.isSuggestion === false);
+    const suggestions = UsersAccounts.filter(user => user.props.isSuggestion === true)
+
     return (
         <div className="c-sidebar">
-            <section className="c-sidebar__user">
+            <section className="c-sidebar__user u-center-x u-display-flex">
                 <ul>
-                    <li>User</li>
+                    {currentUser.map(account => <li>{account}</li>)}
                 </ul>
             </section>
-            <section className="c-sidebar__suggestions">
-                <ul>
-                    <li>Suggestion</li>
-                    <li>Suggestion</li>
-                    <li>Suggestion</li>
-                    <li>Suggestion</li>
-                </ul>
+            <section className="c-sidebar__box-suggestions">
+                <div className="u-space-between-center u-display-flex">
+                    <h1>Sugestões para você</h1>
+                    <h2>Ver tudo</h2>
+                </div>
+                <div className="c-sidebar__suggestion">
+                    <ul>
+                        {suggestions.map(account => <li>{account}</li>)}
+                    </ul>
+                </div>
             </section>
         </div>
     )
