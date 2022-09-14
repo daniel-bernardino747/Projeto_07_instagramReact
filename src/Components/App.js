92d9529c5a5666bf4fc191c1afd233a5bf8b3736
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-09-14 00:09:18 -0300

    componentization and navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,9 @@
+import NavBar from "./NavBar"
+
 export default function App() {
     return (
-        <div className="App">
-            <h1>Hello CodeSandbox</h1>
-            <h2>Start editing to see some magic happen!</h2>
-        </div>
+        <main className="c-main">
+            <NavBar />
+        </main>
     );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,35 @@
+const navItens = [
+    <ion-icon name="paper-plane-outline"></ion-icon>,
+    <ion-icon name="compass-outline"></ion-icon>,
+    <ion-icon name="heart-outline" ></ion-icon>,
+    <ion-icon name="person-outline"></ion-icon>
+]
+
+
+export default function NavBar() {
+    return (
+        <header className="c-main__header u-center-xy u-display-flex">
+
+            <div className="u-width-max-935 u-space-between-center u-display-flex">
+
+                <div className="u-center-xy u-display-flex">
+
+                    <ion-icon name="logo-instagram"></ion-icon>
+                    <img
+                        src="https://logodownload.org/wp-content/uploads/2017/04/instagram-logo-1.png"
+                        alt="Workmark Instagram"
+                    />
+
+                </div>
+
+                <forms>
+                    <input type="text" placeholder="Pesquisar"></input>
+                </forms>
+
+                <ul className="u-display-flex">
+                    {navItens.map(item => <li>{item}</li>)}
+                </ul>
+            </div>
+        </header>
+    )
+}
