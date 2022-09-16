8d2e0aca5b134b633d9a2dba8b85e140d5bb8fc3
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2022-09-15 23:54:01 -0300

    change names, eliminating repetitions

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,38 +1,61 @@
-function Post() {
+function Header(props) {
+    return (
+        <>
+            <div className="c-post__header u-space-between-center u-display-flex">
+                <div className="u-space-between-center u-display-flex">
+                    <img
+                        src={props.photo}
+                        alt="User Perfil Image"
+                    />
+                    <h1>{props.name}</h1>
+                </div>
+                <ion-icon name="ellipsis-horizontal"></ion-icon>
+            </div>
+        </>
+    )
+}
+
+function Footer() {
+    const icons = [
+        <ion-icon name="heart-outline"></ion-icon>,
+        <ion-icon name="chatbubble-outline"></ion-icon>,
+        <ion-icon name="paper-plane-outline"></ion-icon>
+    ]
+
+    return (
+        <>
+            <div className="c-post__footer">
+                <div className="u-space-between-center u-display-flex">
+                    <ul className="u-display-flex">
+                        {icons.map((i) => <li>{i}</li>)}
+                    </ul>
+                    <ion-icon name="bookmark-outline"></ion-icon>
+                </div>
+                <div className="u-center-x u-display-flex">
+                    <img
+                        src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
+                        alt="User Perfil Image"
+                    />
+                    <p>Curtido por <b>daniel__bernardino</b> e <b>outras 345 pessoas</b></p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function Post(props) {
     return (
         <>
             <section className="c-post">
-                <div className="c-post__header u-space-between-center u-display-flex">
-                    <div className="u-space-between-center u-display-flex">
-                        <img
-                            src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                            alt="User Perfil Image"
-                        />
-                        <h1>rodrigo_faro</h1>
-                    </div>
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-                <img
-                    src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                    alt="User Perfil Image"
+                <Header
+                    photo={props.urlPerfil}
+                    name={props.name}
                 />
-                <div className="c-post__footer">
-                    <div className="u-space-between-center u-display-flex">
-                        <ul className="u-display-flex">
-                            <li><ion-icon name="heart-outline"></ion-icon></li>
-                            <li><ion-icon name="chatbubble-outline"></ion-icon></li>
-                            <li><ion-icon name="paper-plane-outline"></ion-icon></li>
-                        </ul>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                    <div className="u-center-x u-display-flex">
-                        <img
-                            src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                            alt="User Perfil Image"
-                        />
-                        <p>Curtido por <b>daniel__bernardino</b> e <b>outras 345 pessoas</b></p>
-                    </div>
-                </div>
+                <img
+                    src={props.urlPost}
+                    alt="Post Image"
+                />
+                <Footer />
             </section>
         </>
     )
@@ -40,9 +63,21 @@ function Post() {
 
 export default function Posts() {
     const posts = [
-        <Post />,
-        <Post />,
-        <Post />
+        <Post
+            name='daniel__bernardino'
+            urlPost='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+            urlPerfil='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Post
+            name='daniel__bernardino'
+            urlPost='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+            urlPerfil='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Post
+            name='daniel__bernardino'
+            urlPost='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+            urlPerfil='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />
     ]
 
     return (
--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -1,111 +1,59 @@
 import borderBackground from '../images/stories_background.png'
 
+function Story(props) {
+    return (
+        <>
+            <div className="u-position-relative">
+                <img
+                    className="c-stories__border"
+                    src={borderBackground}
+                    alt="Stories"
+                />
+                <img
+                    className="c-stories__photo"
+                    src={props.urlPhoto}
+                    alt="Stories"
+                />
+                <p>{props.name}</p>
+            </div>
+        </>
+    )
+}
+
 export default function Stories() {
     const items = [
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>,
-        <div className="u-position-relative">
-            <img
-                className="c-stories__border"
-                src={borderBackground}
-                alt="Stories"
-            />
-            <img
-                className="c-stories__photo"
-                src="https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222"
-                alt="Stories"
-            />
-            <p>Nom </p>
-        </div>
+        <Story
+            name='Rodrigo'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Bernardo'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Julio'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Paulo'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Daniel'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Otávio'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='Mestre'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />,
+        <Story
+            name='João'
+            urlPhoto='https://i.scdn.co/image/ab6761610000e5eb207c6849d1a1f4480e6aa222'
+        />
     ]
 
     return (
